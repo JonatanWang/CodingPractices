239af88ee786d63a708cc5695d078e6f737f48c1
zw <zhengyuw@kth.se>
2019-05-12 19:12:51 +0200

    Initial commit

---
+++ src/StackAndQueue/StackWithGetMin.java
@@ -0,0 +1,58 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+/**
+ * Implement a special stack,
+ * in addition to push&pop operations,
+ * which can return the minimal element in it;
+ *
+ * Time complexity O(1) for push, pop & getMin().
+ */
+public class StackWithGetMin extends Stack <Integer> {
+
+    private Stack<Integer> minStack;
+
+    public StackWithGetMin() {
+
+        minStack = new Stack<>();
+    }
+
+    public Integer push(Integer e) {
+
+        if (minStack.empty()) {
+            minStack.push(e);
+        }
+
+        if (minStack.peek() >= e) {
+
+            minStack.push(e);
+        }
+
+        return super.push(e);
+    }
+
+    public Integer pop() {
+
+        if (empty()) {
+
+            throw new RuntimeException("The stack is empty.");
+        }
+        int rst = super.pop();
+        if (rst == minStack.peek()) {
+
+            minStack.pop();
+        }
+
+        return rst;
+    }
+
+    public Integer getMin() {
+
+        if (minStack.empty()) {
+
+            throw new RuntimeException("The stack is empty.");
+        }
+        return minStack.peek();
+    }
+}
